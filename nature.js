836899435636c7476d52c5ff45ba2c274cdbd6ee
harshadapr <harshadapraundal97@gmail.com>
2023-01-08 21:33:00 +0530

    pages

--- nature.js
+++ nature.js
@@ -1,33 +1,59 @@
+let userdata = JSON.parse(localStorage.getItem("user-info"));
+console.log(userdata);
+if (userdata != null) {
+    console.log(userdata)
+    document.getElementById("user_name").innerHTML = userdata.user;
+    document.getElementById("login_user").style.display = "block";
+    document.getElementById("logout_user").style.display = "none";
+}
+document.getElementById("user").addEventListener("click", () => {
+
+    document.getElementById("user_popup").style.display = "block"
+})
+
+document.getElementById("logout").addEventListener("click", () => {
+    localStorage.setItem("user-info", JSON.stringify({}));
+})
+document.querySelector(".search_header").addEventListener("input", () => {
+    deboubcing(getData, 2000);
+
+})
+//---------------------------------------------------------------------------------
+
 let id;
-const apiKey = `wO6nmBgN1v66z9W-6DGCDsIV4GQvBf-xW8rMmv-HA70`;
-const searchurl = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=nature&per_page=30`;
+let searchval;
+const apiKey = `aBSiI0ek7J1gkMe2xa8JkLlHdB9UlqUBDNFjUnYMWtg`;
+const url = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=nature&per_page=30`;
 let curr = 0;
-// let searchval;
 let imgData;
 const container = document.getElementById("content");
-// const deboubcing = (fn, delay) => {
-//     if (id != undefined) {
-//         clearTimeout(id);
-//     }
-//     id = setTimeout(() => {
-//         searchval = document.getElementById("searchv").value;
 
-//         console.log(searchval)
-//         fn(searchurl);
-//     }, delay)
-// }
+const deboubcing = (fn, delay) => {
+    if (id != undefined) {
+        clearTimeout(id);
+    }
+    id = setTimeout(() => {
+
+        searchval = document.querySelector(".search_header").value;
+
+        const searchurl = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=${searchval}&per_page=30`;
+
+        console.log(searchval)
+        fn(searchurl);
+    }, delay)
+}
 
 const getData = async (fetchurl) => {
-
+    console.log(fetchurl)
     let rem = await fetch(fetchurl);
     let data = await rem.json();
     console.log(data);
     // if (searchval == undefined) {
 
-    //     imgData = data;
-    // } {
+    //     imgData = data.re;
+    // } else {
     imgData = data.results
-
+    // }//                              
     display(imgData);
 
 
@@ -37,12 +63,14 @@ const display = (data) => {
     data.map((ele, i) => {
         let div = document.createElement("div");
         let img = document.createElement("img");
+
         img.src = ele.urls.regular;
         div.id = 'card_img';
         img.onclick = () => {
             curr = i;
             popupData(ele);
         }
+
         div.append(img)
         container.append(div);
     })
@@ -55,7 +83,8 @@ const popupData = (ele) => {
     let img = document.querySelector('.download_img');
 
     popu.classList.remove('hide');
-    download.href = ele.links.html;
+
+    download.href = ele.links.download_location;
     img.src = ele.urls.regular;
     crossbtn.addEventListener("click", () => {
         popu.classList.add('hide');
@@ -77,4 +106,4 @@ document.querySelector(".next_button").addEventListener("click", () => {
 
 
 
-getData(searchurl);
+getData(url);
--- wallpaper.js
+++ wallpaper.js
@@ -1,92 +1,109 @@
-// let id;
-// import { getData, display, popupData, id, curr, imgData, getData } from "./component/component.js";
-import { getData, display, popupData, id, curr, imgData } from "./component/component.js";
-const apiKey = `wO6nmBgN1v66z9W-6DGCDsIV4GQvBf-xW8rMmv-HA70`;
-const searchurl = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=Wallpapers&per_page=30`;
-let getdAta = getData;
-let Display = display;
-let PopupData = popupData;
+let userdata = JSON.parse(localStorage.getItem("user-info"));
+console.log(userdata);
+if (userdata != null) {
+    console.log(userdata)
+    document.getElementById("user_name").innerHTML = userdata.user;
+    document.getElementById("login_user").style.display = "block";
+    document.getElementById("logout_user").style.display = "none";
+}
+document.getElementById("user").addEventListener("click", () => {
+
+    document.getElementById("user_popup").style.display = "block"
+})
+
+document.getElementById("logout").addEventListener("click", () => {
+    localStorage.setItem("user-info", JSON.stringify({}));
+})
+document.querySelector(".search_header").addEventListener("input", () => {
+    deboubcing(getData, 2000);
+
+})
+//---------------------------------------------------------------------------------
+
+let id;
+let searchval;
+const apiKey = `aBSiI0ek7J1gkMe2xa8JkLlHdB9UlqUBDNFjUnYMWtg`;
+const url = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=wallpapers&per_page=30`;
+let curr = 0;
+let imgData;
 const container = document.getElementById("content");
-// let Imgdata = imgData;
-// let id1 = id;
-// let curr1 = curr;
-getdAta(searchurl);
-Display();
-PopupData();
-// let curr = 0;
-// // let searchval;
-// let imgData;
-// const container = document.getElementById("content");
-// // const deboubcing = (fn, delay) => {
-// //     if (id != undefined) {
-// //         clearTimeout(id);
-// //     }
-// //     id = setTimeout(() => {
-// //         searchval = document.getElementById("searchv").value;
 
-// //         console.log(searchval)
-// //         fn(searchurl);
-// //     }, delay)
-// // }
+const deboubcing = (fn, delay) => {
+    if (id != undefined) {
+        clearTimeout(id);
+    }
+    id = setTimeout(() => {
 
-// const getData = async (fetchurl) => {
+        searchval = document.querySelector(".search_header").value;
 
-//     let rem = await fetch(fetchurl);
-//     let data = await rem.json();
-//     console.log(data);
-//     // if (searchval == undefined) {
+        const searchurl = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=${searchval}&per_page=30`;
 
-//     //     imgData = data;
-//     // } {
-//     imgData = data.results
+        console.log(searchval)
+        fn(searchurl);
+    }, delay)
+}
 
-//     display(imgData);
+const getData = async (fetchurl) => {
+    console.log(fetchurl)
+    let rem = await fetch(fetchurl);
+    let data = await rem.json();
+    console.log(data);
+    // if (searchval == undefined) {
+
+    //     imgData = data.re;
+    // } else {
+    imgData = data.results
+    // }//                              
+    display(imgData);
 
 
-// }
-// const display = (data) => {
-//     container.innerHTML = null;
-//     data.map((ele, i) => {
-//         let div = document.createElement("div");
-//         let img = document.createElement("img");
-//         img.src = ele.urls.regular;
-//         div.id = 'card_img';
-//         img.onclick = () => {
-//             curr = i;
-//             popupData(ele);
-//         }
-//         div.append(img)
-//         container.append(div);
-//     })
-// }
-// const popupData = (ele) => {
-//     // console.log("yes");
-//     let popu = document.getElementById("popu");
-//     const crossbtn = document.querySelector('.button_c');
-//     const download = document.querySelector(".download");
-//     let img = document.querySelector('.download_img');
+}
+const display = (data) => {
+    container.innerHTML = null;
+    data.map((ele, i) => {
+        let div = document.createElement("div");
+        let img = document.createElement("img");
 
-//     popu.classList.remove('hide');
-//     download.href = ele.links.html;
-//     img.src = ele.urls.regular;
-//     crossbtn.addEventListener("click", () => {
-//         popu.classList.add('hide');
-//     })
-// }
+        img.src = ele.urls.regular;
+        div.id = 'card_img';
+        img.onclick = () => {
+            curr = i;
+            popupData(ele);
+        }
+
+        div.append(img)
+        container.append(div);
+    })
+}
+const popupData = (ele) => {
+    // console.log("yes");
+    let popu = document.getElementById("popu");
+    const crossbtn = document.querySelector('.button_c');
+    const download = document.querySelector(".download");
+    let img = document.querySelector('.download_img');
+
+    popu.classList.remove('hide');
+
+    download.href = ele.links.download_location;
+    img.src = ele.urls.regular;
+    crossbtn.addEventListener("click", () => {
+        popu.classList.add('hide');
+    })
+}
 
 document.querySelector(".prev_button").addEventListener("click", () => {
     if (curr > 0) {
         curr--;
-        PopupData(imgData[curr]);
+        popupData(imgData[curr]);
     }
 });
 document.querySelector(".next_button").addEventListener("click", () => {
     if (curr < imgData.length - 1) {
         curr++;
-        PopupData(imgData[curr]);
+        popupData(imgData[curr]);
     }
 });
 
 
 
-// getData(searchurl);
+getData(url);
